f9da9a060b1147edbeaabf61e059512e1c2f946c
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-02-25 21:10:15 -0800

    chore: update domain

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? "rushii.materii.dev",
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? "blog.rushii.dev",
   title: 'rushii',
   subtitle: 'personal blog for dumb shit',
   lang: 'en-US',
@@ -10,7 +10,7 @@ export const site: SiteConfig = {
   author: {
     avatar: "https://avatars.githubusercontent.com/u/33725716?v=4",
     name: 'rushii',
-    bio: 'i love reverse engineering android apps, jetpack compose, and kotlin',
+    bio: 'I love reverse engineering android apps, jetpack compose, and kotlin',
     metadata: [
       {
         icon: "i-simple-icons-github",
